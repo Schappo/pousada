17d806c07a191ef56941522a1c2fc5f5e76111cf
Felipe Schappo <felipe@gobacklog.com>
2021-11-28 08:40:41 -0300

    update

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,26 +1,88 @@
 import React from 'react'
-import { Container, Image, Row, Col } from 'react-bootstrap'
+import { Button, Container, Image, Row, Col, Card, Carousel } from 'react-bootstrap'
 import Logo from '../../assets/img/logo/logo.png'
 
 const Home: React.FC = () => {
   return (
-    <>
-      <div>
-        <Container fluid className="flex-center home">
-          <Image src={Logo} className="img-presentation" alt="Residencial Schappo" />
+    <div className="home">
+      <div className="home-el">
+        <Container fluid className="flex-center">
+          <Row className="logo-options">
+            <Col>
+              <Image src={Logo} className="img-presentation" alt="Residencial Schappo" />
+            </Col>
+            <Col>
+              <Row>
+                <Col>
+                  <Card style={{ width: '18rem' }}>
+                    <Carousel>
+                      <Carousel.Item>
+                        <img
+                          className="d-block w-100"
+                          src="holder.js/800x400?text=First slide&bg=373940"
+                          alt="First slide"
+                        />
+                        <Carousel.Caption>
+                          <h3>First slide label</h3>
+                          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                        </Carousel.Caption>
+                      </Carousel.Item>
+                      <Carousel.Item>
+                        <img
+                          className="d-block w-100"
+                          src="holder.js/800x400?text=Second slide&bg=282c34"
+                          alt="Second slide"
+                        />
+
+                        <Carousel.Caption>
+                          <h3>Second slide label</h3>
+                          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        </Carousel.Caption>
+                      </Carousel.Item>
+                      <Carousel.Item>
+                        <img
+                          className="d-block w-100"
+                          src="holder.js/800x400?text=Third slide&bg=20232a"
+                          alt="Third slide"
+                        />
+
+                        <Carousel.Caption>
+                          <h3>Third slide label</h3>
+                          <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                        </Carousel.Caption>
+                      </Carousel.Item>
+                    </Carousel>
+                    <Card.Body>
+                      <Card.Title>Card Title</Card.Title>
+                      <Card.Text>
+                        {
+                          `Some quick example text to build on the card title and make up the bulk of
+                          the card's content.`
+                        }
+                      </Card.Text>
+                      <Button variant="primary">Go somewhere</Button>
+                    </Card.Body>
+                  </Card>
+                </Col>
+                <Col>
+                  Reservas
+                </Col>
+              </Row>
+            </Col>
+          </Row>
         </Container>
       </div>
-      <div className="intro">
-        <Container className="flex-center intro">
+      <div className="home-el slogan">
+        <Container className="flex-center">
           <Row>
-            <Col><h4 className="intro-rs">Residencial Schappo</h4></Col>
+            <Col><h4 className="home-rs">Residencial Schappo</h4></Col>
           </Row>
           <Row>
-            <Col><h1 className="intro-slogan">Suas férias em Florianópolis começam aqui</h1></Col>
+            <Col><h1 className="home-slogan">Suas férias em Florianópolis começam aqui</h1></Col>
           </Row>
         </Container>
       </div>
-    </>
+    </div>
   )
 }
 
